252442c29383d67b619b50c8a632284540007bda
xDroni <32304210+xDroni@users.noreply.github.com>
2020-05-25 00:48:04 +0200

    added auto report indexer, for users that have grepodata userscript installed

--- index.user.js
+++ index.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name                Grepolis Sending Resources Hotkeys
-// @version             1.0.1
+// @version             1.0.2
 // @author              dx droni <mrdroonix@gmail.com>
 // @updateURL           https://github.com/xDroni/Grepolis-Resources-Hotkeys/raw/master/index.user.js
 // @downloadURL         https://github.com/xDroni/Grepolis-Resources-Hotkeys/raw/master/index.user.js
@@ -12,11 +12,13 @@
 // @grant               none
 // ==/UserScript==
 
+// global variables
 let townSwitchInputToogle = null;
 
 (() => {
    tradeHotkeys();
    townSwitch();
+   reportAutoIndexer();
 })()
 
 function tradeHotkeys() {
@@ -163,5 +165,47 @@ function townSwitch() {
 
     function goToTown(townId) {
         HelperTown.townSwitch(townId)
+    }
+}
+
+// Automatically index opened reports. Works only if you have GrepoData user script installed
+async function reportAutoIndexer() {
+
+    let lastReportElement = null;
+
+    const observer = new MutationObserver((mutations) => {
+        const reportWindow = document.getElementById('report_report');
+        const mutatedElement = mutations[0].target;
+
+        // only if report element is not null, ajax loader is hidden (report is loaded) and report element is different than the last one
+        if(reportWindow && mutatedElement.style.visibility === 'hidden' && reportWindow !== lastReportElement) {
+            lastReportElement = reportWindow;
+
+            const indexElement = document.getElementById('gd_index_rep_txt');
+
+            // if index element is not null and it's not indexed yet (textContent === Index +)
+            if(indexElement && indexElement.textContent === 'Index +') {
+                indexElement.click()
+            }
+        }
+    })
+
+    await waitUntilElementIsLoaded('ajax_loader', 1000);
+
+    const ajaxLoader = document.getElementById('ajax_loader')
+
+    observer.observe(ajaxLoader, {
+        attributes: true,
+    })
+
+    function wait(time) {
+        return new Promise(resolve => setTimeout(resolve, time));
+    }
+
+    async function waitUntilElementIsLoaded(selector, time) {
+        if(document.getElementById(selector) === null) {
+            await wait(time);
+            await waitUntilElementIsLoaded(selector, time);
+        }
     }
 }
