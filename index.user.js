2d992f814f9d9a7e923bc442e5737bf6c6febc56
xDroni <32304210+xDroni@users.noreply.github.com>
2020-05-24 02:47:19 +0200

    added town switcher by name (default hotkey: Ctrl + V)

--- index.user.js
+++ index.user.js
@@ -12,75 +12,156 @@
 // @grant               none
 // ==/UserScript==
 
-window.addEventListener('keydown', event => {
-    if(event.target.tagName.toUpperCase() === 'INPUT') {
-        const resourcesInputElements = document.querySelectorAll('.resource_selector > div.spinner > div.body > input')
-        if(resourcesInputElements.length > 0) {
-            const activeElement = document.activeElement
-            const array = Array.from(resourcesInputElements)
-            if(array.includes(activeElement)) {
-                const index = array.findIndex(element => element === activeElement)
-                switch (event.key) {
-                    // unfocus the input using Escape key
-                    case 'Escape': {
-                        event.preventDefault()
-                        activeElement.blur()
-                        break
-                    }
+let townSwitchInputToogle = null;
 
-                    // select the next resource input
-                    case 'Tab': {
-                        event.preventDefault()
+(() => {
+   tradeHotkeys();
+   townSwitch();
+})()
 
-                        // change the focus (active element)
-                        // mod 3 because we have 3 resource types, if we have 3rd focused clicking Tab will focus the 1st one
-                        array[(index+1) % 3].focus()
-                        break
-                    }
+function tradeHotkeys() {
+    window.addEventListener('keydown', event => {
+        if(event.target.tagName.toUpperCase() === 'INPUT') {
+            const resourcesInputElements = document.querySelectorAll('.resource_selector > div.spinner > div.body > input')
+            if(resourcesInputElements.length > 0) {
+                const activeElement = document.activeElement
+                const array = Array.from(resourcesInputElements)
+                if(array.includes(activeElement)) {
+                    const index = array.findIndex(element => element === activeElement)
+                    switch (event.key) {
+                        // unfocus the input using Escape key
+                        case 'Escape': {
+                            event.preventDefault()
+                            activeElement.blur()
+                            break
+                        }
 
-                    // send the resources
-                    case 'Enter': {
-                        event.preventDefault()
-                        const sendButton = document.querySelector('#trade > div > div.content > div.btn_trade_button.button_new')
-                        sendButton.click()
-                        activeElement.blur()
-                        break
-                    }
-
-                    default: {
-                        const keyValue = parseInt(event.key)
-                        if(!isNaN(keyValue)) { // if not NaN
+                        // select the next resource input
+                        case 'Tab': {
                             event.preventDefault()
 
-                            // treat 0 by default
-                            if(keyValue === 0) {
-                                activeElement.value = activeElement.value * 10
+                            // change the focus (active element)
+                            // mod 3 because we have 3 resource types, if we have 3rd focused clicking Tab will focus the 1st one
+                            array[(index+1) % 3].focus()
+                            break
+                        }
 
-                                // unfocus the element and focus it back to apply the value
-                                // without this there was a problem with adding resources using arrow keys (feature provided by Potusek Report Converter)
+                        // send the resources
+                        case 'Enter': {
+                            event.preventDefault()
+                            const sendButton = document.querySelector('#trade > div > div.content > div.btn_trade_button.button_new')
+                            sendButton.click()
+                            activeElement.blur()
+                            break
+                        }
+
+                        default: {
+                            const keyValue = parseInt(event.key)
+                            if(!isNaN(keyValue)) { // if not NaN
+                                event.preventDefault()
+
+                                // treat 0 by default
+                                if(keyValue === 0) {
+                                    activeElement.value = activeElement.value * 10
+
+                                    // unfocus the element and focus it back to apply the value
+                                    // without this there was a problem with adding resources using arrow keys (feature provided by Potusek Report Converter)
+                                    activeElement.blur()
+                                    activeElement.focus()
+                                    break
+                                }
+
+
+                                activeElement.value = keyValue * 1000
+
                                 activeElement.blur()
                                 activeElement.focus()
-                                break
                             }
-
-
-                            activeElement.value = keyValue * 1000
-
-                            activeElement.blur()
-                            activeElement.focus()
                         }
                     }
                 }
             }
         }
-    }
-    else {
-        if(event.key === 'Tab') {
-            event.preventDefault()
-            const resourcesInputElements = document.querySelectorAll('.resource_selector > div.spinner > div.body > input')
-            if(resourcesInputElements.length > 0) {
-                resourcesInputElements[0].focus()
+        else {
+            switch (event.code) {
+                case 'Tab': {
+                    event.preventDefault();
+                    const resourcesInputElements = document.querySelectorAll('.resource_selector > div.spinner > div.body > input');
+                    if (resourcesInputElements.length > 0) {
+                        resourcesInputElements[0].focus();
+                    }
+                    break;
+                }
             }
         }
+    });
+}
+
+function townSwitch() {
+    window.addEventListener('keydown', (event) => {
+        if ((event.target.tagName.toUpperCase() !== 'INPUT' && event.target.tagName.toUpperCase() !== 'TEXTAREA') || document.activeElement === document.getElementById('townSwitch')) {
+            switch (event.code) {
+                case 'KeyV' : {
+                    // trigger only if Control key is pressed
+                    if (event.ctrlKey) {
+                        event.preventDefault();
+                        if (townSwitchInputToogle === null) {
+                            let townSwitch = document.createElement('input');
+                            townSwitch.id = 'townSwitch';
+                            townSwitch.style = 'margin-top: 20px;';
+                            let element = document.querySelector('.culture_overview_wrapper');
+                            element.appendChild(townSwitch);
+                            townSwitch.focus();
+                            townSwitchInputToogle = true;
+                        } else {
+                            const townSwitch = document.getElementById('townSwitch');
+                            if (townSwitchInputToogle) {
+                                hideInput(townSwitch);
+                            } else {
+                                showInput(townSwitch);
+
+                            }
+                            townSwitchInputToogle = !townSwitchInputToogle;
+                        }
+                    }
+                    break;
+                }
+                case 'Enter': {
+                    let townSwitch = document.getElementById('townSwitch');
+
+                    // check if townSwitch is declared has focus
+                    if (townSwitch && document.activeElement === townSwitch && townSwitch.value) {
+                        const townId = searchForTown(townSwitch.value);
+                        hideInput(townSwitch);
+                        townSwitchInputToogle = !townSwitchInputToogle;
+                        goToTown(townId);
+                    }
+                }
+            }
+        }
+    });
+
+    function hideInput(input) {
+        input.style = 'display: none';
+        input.value = '';
+        input.blur();
     }
-})
+
+    function showInput(input) {
+        input.style = 'margin-top: 20px';
+        input.focus();
+    }
+
+    function searchForTown(text) {
+        const townsObject = ITowns.getTowns();
+        for (let [id, townObject] of Object.entries(townsObject)) {
+            const { name } = townObject
+            if(name.toLowerCase().includes(text.toLowerCase())) return id;
+        }
+        return null;
+    }
+
+    function goToTown(townId) {
+        HelperTown.townSwitch(townId)
+    }
+}
